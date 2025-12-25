73ae69eee0ead901e3b7988f8c3370309df8c9b9
daralkoraniserlohn <daralkoraniserlohn@gmail.com>
2025-12-25 03:23:26 +0100

    Update CACHE_VERSION to v1.0.9

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    كل ما تغيّر CACHE_VERSION أو CACHE_NAME → يتم حذف كل الكاشات القديمة تلقائياً في activate
 */
 
-const CACHE_VERSION = "v1.0.8"; // غيّرها عند كل تحديث
+const CACHE_VERSION = "v1.0.9"; // غيّرها عند كل تحديث
 const CACHE_NAME = `masjid-app-cache-${CACHE_VERSION}`;
 
 // عدّل الملفات اللي تريدها تتخزن Offline
